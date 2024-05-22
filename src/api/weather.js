6834f7277ef01e483b2b90d6c418875eb49a7bd3
郭方超 <guofangchao@idongwo.com>
2024-05-22 21:13:13 +0800

    add

--- src/api/weather.js
+++ src/api/weather.js
@@ -4,7 +4,7 @@ import { GetWeatherNow, GetGeoApi } from '../service/hefeng.js';
 
 const router = new Router();
 
-//配置列表
+//获取当前位置城市
 router.get('/', async function (ctx) {
     try {
         const data = await GetGeoApi('116.45,39.90');
@@ -13,5 +13,18 @@ router.get('/', async function (ctx) {
         ctx.body = BeError(error.message);
     }
 });
+
+//获取对应城市当前天气
+// 未来天气预报
+//小时级天气预报
+//高精度格点天气
+//灾害预警
+//天气指数
+//空气质量
+//历史天气
+//台风预报
+//海洋预报
+//太阳辐射
+//天文预报
 
 export default router.routes();
